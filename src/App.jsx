4ee455499486a3c613b1388a16c7cae5d6061179
miguelgisbert12 <miguelgisbert12@alumnos.cei.es>
2026-08-18 19:24:09 +0200

    Creado el sistema de rutas inicial con Browser Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Compendium from './pages/Compendium'
 import NotFound from './pages/NotFound'
+import MainLayout from './layouts/MainLayout'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/compendium" element={<Compendium />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/compendium" element={<Compendium />} />
+        </Route>
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
