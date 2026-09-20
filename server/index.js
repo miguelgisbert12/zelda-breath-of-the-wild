b800eb8e0828c29cc876f5349f617667e8987d08
miguelgisbert12 <miguelgisbert12@alumnos.cei.es>
2026-09-20 19:47:28 +0200

    Solución de errores

--- server/index.js
+++ server/index.js
@@ -57,6 +57,6 @@ app.get('/api/admin/health', requireAuth, requireAdmin, (req, res) => {
   })
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor backend ejecutándose en http://localhost:${PORT}`)
 })
